47833309763471bd355f94938ad393f6916ef908
lkj10 <lee_10_@naver.com>
2021-04-13 23:02:36 +0900

    added_get_last_num
    최신 회차 번호 가져오는 함수

--- main.py
+++ main.py
@@ -8,12 +8,22 @@ main_url = "https://www.dhlottery.co.kr/gameResult.do?method=byWin"
 # 임의의 회차를 얻기 위한 주소
 basic_url = "https://www.dhlottery.co.kr/gameResult.do?method=byWin&drwNo=" 
 
-#마지막 회차 번호 가저오는 함수
-#def get_last_num():
-request = requests.get(main_url)
-soup = BeautifulSoup(request.text, 'html.parser')
-num = soup.find("h4").text
-print(num)
+#마지막 회차 번호 가져오는 함수
+def get_last_num():
+    num_list = []
+    request = requests.get(main_url)
+    soup = BeautifulSoup(request.text, 'html.parser')
+    num = soup.find("h4").text
+    for i in num:
+        try: 
+            if(int(i)):
+                num_list.append(i)
+        except: 
+            pass
+    lotto_num = ''.join(num_list)
+
+    return lotto_num
+
 
 
 #특정회차 로또 번호 알려주는 함수
